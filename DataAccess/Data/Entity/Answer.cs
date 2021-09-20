ff49cefbb57dacfd7b2f9fd6577b7b2557c1f4c9
yazan.codavia <yazankassam.codavia@gmail.com>
2021-09-20 15:14:33 +0300

    answer for assey question

---
+++ DataAccess/Data/Entity/Answer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using iread_assignment_ms.DataAccess.Data.Type;
+
+namespace iread_assignment_ms.DataAccess.Data.Entity
+{
+    public class Answer
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AnswerId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+
+        [EnumDataType(typeof(QuestionType))]
+        public string Type { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+        public string StudentId { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string StudentFirstName { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string StudentLastName { get; set; }
+        public bool IsAnswered { get; set; }
+
+
+
+    }
+}
---
+++ DataAccess/Data/Entity/EssayAnswer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace iread_assignment_ms.DataAccess.Data.Entity
+{
+    public class EssayAnswer : Answer
+    {
+
+        public string Text { get; set; }
+
+    }
+}

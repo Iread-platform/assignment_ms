b086521f4fb94a4bb6551acd1e41cc6a5eadcdf5
codavia_kinda <codavia.kinda.flihan@gmail.com>
2021-09-21 11:29:27 +0300

    IR-223 Add date validation when craete assignment.

--- Web/Dto/AssignmentDto/AssignmentCreateDto.cs
+++ Web/Dto/AssignmentDto/AssignmentCreateDto.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using iread_assignment_ms.Web.Dto.AttachmentDto;
 using iread_assignment_ms.Web.DTO.StoryDto;
+using iread_assignment_ms.Web.Util;
 
 namespace iread_assignment_ms.Web.Dto
 {
     public class AssignmentCreateDto
     {
         [Required]
+        [DateValidation.NotDefaultValueAttribute(ErrorMessage = ErrorMessage.START_DATE_REQUIRED)]
+        [DateValidation.AfterNowAttribute(ErrorMessage = ErrorMessage.START_DATE_AFTER_NOW)]
         public DateTime StartDate { get; set; }
+        
         [Required]
+        [DateValidation.NotDefaultValueAttribute(ErrorMessage = ErrorMessage.END_DATE_REQUIRED)]
+        [DateValidation.AfterNowAttribute(ErrorMessage = ErrorMessage.END_DATE_AFTER_NOW)]
+        [DateValidation.AfterAttribute(StartDateProperty = "StartDate", ErrorMessage = ErrorMessage.END_DATE_AFTER_START)]
         public DateTime EndDate { get; set; }
+        
         [Required]
         public Nullable<int> ClassId { get; set; }
 
--- Web/Util/ErrorMessage.cs
+++ Web/Util/ErrorMessage.cs
@@ -16,6 +16,11 @@ namespace iread_assignment_ms.Web.Util
         public const String Audio_ID_IS_REQUIRED = "Audio id is required.";
         public const String INVALID_Audio_ID_VALUE = "Invalid audio id value.";
         public const String FILE_EXTENSION_NOT_ALLOWED = "File extension is not allowed!.";
+        public const String START_DATE_AFTER_NOW = "The start date should be now or after the current date.";
+        public const String START_DATE_REQUIRED = "Start date is required.";
+        public const String END_DATE_AFTER_NOW = "The end date should be after the current date.";
+        public const String END_DATE_REQUIRED = "End date is required.";
+        public const String END_DATE_AFTER_START = "The end date should be after the start date.";
         public const String TAG_TITLE_REQUIRED = "Tag title is required.";
 
 

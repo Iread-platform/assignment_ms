51be1c7c4e2065fea3b20ea27af2c439c5dbc207
yazan.codavia <yazankassam.codavia@gmail.com>
2021-09-26 15:18:12 +0300

    entity and repo

---
+++ DataAccess/Data/Entity/FeedBack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iread_assignment_ms.DataAccess.Data.Entity
+{
+    public class FeedBack
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FeedBackId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Text { get; set; }
+
+
+
+        public int AnswerId { get; set; }
+        public Answer Answer { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        public string StudentId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string TeacherId { get; set; }
+
+
+
+    }
+}
